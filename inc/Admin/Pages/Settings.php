920594d31588c6c5d93e6eff15a59b468d15f751
Mak Alamin <makalamin012@gmail.com>
2023-03-27 04:01:20 +0600

    designed crb tabs

--- inc/Admin/Pages/Settings.php
+++ inc/Admin/Pages/Settings.php
@@ -13,35 +13,45 @@ class Settings
 {
     public function register()
     {
-        add_action('carbon_fields_register_fields', [$this, 'attach_plugin_options']);
+        add_action('carbon_fields_register_fields', [$this, 'generate_options_with_carbon_fields']);
     }
 
-    function attach_plugin_options()
+    function generate_options_with_carbon_fields()
     {
+        $weekdays = array(
+            'montag' => __('Montag', 'wp-liefermanager'),
+            'dienstag' => __('Dienstag', 'wp-liefermanager'),
+            'mittwoch' => __('Mittwoch', 'wp-liefermanager'),
+            'donnerstag' => __('Donnerstag', 'wp-liefermanager'),
+            'freitag' => __('Freitag', 'wp-liefermanager')
+        );
+
+        $opening_hour_fields = array();
+        $delivery_time_fields = array();
+
+        foreach ($weekdays as $slug => $weekday) {
+            $opening_hour_fields[] = Field::make('complex', 'wp_liefer_' . $slug . '_opening_hours', $weekday)
+                ->add_fields(array(
+                    Field::make('time', 'open_at', __('Opens', 'wp-liefermanager')),
+                    Field::make('time', 'close_at', __('Closes', 'wp-liefermanager'))
+                ));
+
+            $delivery_time_fields[] = Field::make('complex', 'wp_liefer_' . $slug . '_delivery_times', $weekday)
+                ->add_fields(array(
+                    Field::make('time', 'open_at', __('Opens', 'wp-liefermanager')),
+                    Field::make('time', 'close_at', __('Closes', 'wp-liefermanager'))
+                ));
+        }
+
         Container::make('theme_options', __('Settings'))
             ->set_page_parent('wp-liefermanager')
 
-            ->add_tab(__('Opening Hours'), array(
-                Field::make('complex', 'wp_liefer_montag_opening_hours', __('Montag'))
-                    ->add_fields(array(
-                        Field::make('time', 'open_at', __('Opens', 'wp-liefermanager')) // ->set_time_format('HH')
-                        ,
-                        Field::make('time', 'close_at', __('Closes', 'wp-liefermanager')) // ->set_time_format('HH')
-                    )),
+            ->add_tab(__('Opening Hours', 'wp-liefermanager'), $opening_hour_fields)
 
-                // Field::make('complex', 'wp_liefer_dienstag_opening_hours', __('Dienstag', 'wp-liefermanager'))
-                //     ->add_fields(array(
-                //         Field::make('time', 'open_at', __('Opens', 'wp-liefermanager'))->set_time_format('HH'),
-                //         Field::make('time', 'close_at', __('Closes', 'wp-liefermanager'))->set_time_format('HH'),
-                //     ))
-            ))
-
-            ->add_tab(__('Delivery Times'), array(
-                Field::make('text', 'crb_delivery_times', __('Add Time', 'wp-liefermanager')),
-            ))
+            ->add_tab(__('Delivery Times'), $delivery_time_fields)
 
             ->add_tab(__('Tip'), array(
-                Field::make('text', 'crb_tip_type', __('Tip Type', 'wp-liefermanager')),
+                Field::make('text', 'wp_liefer_tip_type', __('Tip Type', 'wp-liefermanager')),
             ));
     }
 }
