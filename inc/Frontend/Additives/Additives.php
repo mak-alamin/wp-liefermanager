00ac9fb4f97aa0e349acefb69706f48b14501303
Mak Alamin <makalamin012@gmail.com>
2023-04-12 20:46:46 +0600

    created shortcodes for food types

--- inc/Frontend/Additives/Additives.php
+++ inc/Frontend/Additives/Additives.php
@@ -17,7 +17,7 @@ class Additives
             return;
         }
 
-        add_action('woocommerce_before_add_to_cart_button', [$this, 'show_additives']);
+        add_action('woocommerce_product_meta_end', [$this, 'show_additives']);
     }
 
     public function show_additives()
@@ -29,8 +29,6 @@ class Additives
         echo '<div class="additives">';
 
         if (!empty($allergenes_keys)) {
-            echo '<h4 class="mt-3">Allergene:</h4>';
-
             echo '<ul >';
 
             foreach ($all_allergenes as $key => $allergene) {
@@ -40,7 +38,9 @@ class Additives
 
                     $icon = wp_get_attachment_url($allergene['icon']);
 
-                    echo ' <li>' . $allergene['fullname'] . '<img src="' . $icon . '" class="ml-1"/>' . '</li>';
+                    // echo ' <li>' . $allergene['fullname'] . '<img src="' . $icon . '" class="ml-1"/>' . '</li>';
+
+                    echo ' <li><a href="' . site_url('zusatzstoffe-allergene') . '" target="_blank"> <img src="' . $icon . '" class="ml-1"/></a></li>';
                 }
             }
 
@@ -52,7 +52,7 @@ class Additives
         $additive_keys = get_post_meta(get_the_ID(), 'additives_checked', true);
 
         if (!empty($additive_keys)) {
-            echo '<h4>' . _x('Zusatzstoffe', 'wp-liefermanager') . ':</h4>';
+            // echo '<h4>' . _x('Zusatzstoffe', 'wp-liefermanager') . ':</h4>';
 
             echo '<ul>';
 
@@ -60,9 +60,11 @@ class Additives
                 $additive_key = strtolower(str_replace(' ', '_', $additive['fullname']));
 
                 if (in_array($additive_key, $additive_keys)) {
-                    $icon = wp_get_attachment_url($allergene['icon']);
+                    $icon = wp_get_attachment_url($additive['icon']);
 
-                    echo ' <li>' . $additive['fullname'] . '<img src="' . $icon . '" class="ml-1"/>' . '</li>';
+                    // echo ' <li>' . $additive['fullname'] . '<img src="' . $icon . '" class="ml-1"/>' . '</li>';
+
+                    echo ' <li><a href="' . site_url('zusatzstoffe-allergene') . '" target="_blank"> <img src="' . $icon . '" class="ml-1"/></a></li>';
                 }
             }
 
--- inc/Frontend/Shortcodes/Shortcodes.php
+++ inc/Frontend/Shortcodes/Shortcodes.php
@@ -6,66 +6,69 @@
 
 namespace Inc\Frontend\Shortcodes;
 
-// [lebensmittel_typ]
-
 class Shortcodes
 {
     public function register()
     {
+        if (is_admin()) {
+            return;
+        }
+
         add_shortcode('zusatzstoffe', [$this, 'generate_additives_shortcode']);
 
         add_shortcode('allergene', [$this, 'generate_allergenes_shortcode']);
+
+        add_shortcode('lebensmittel_typ', [$this, 'generate_foodtype_shortcode']);
+    }
+
+    public function generate_shortcode_table($options)
+    {
+        if (empty($options)) {
+            return;
+        }
+
+        echo '<div class="additives">';
+        echo '<table>';
+
+        foreach ($options as $key => $option) {
+
+            $icon = isset($option['icon']) ? wp_get_attachment_url($option['icon']) : '';
+
+            $description = isset($option['description']) ? $option['description'] : '';
+
+            echo '<tr>';
+
+            echo '<td><img src="' . $icon . '" /></td>';
+
+            echo '<td> <strong> ' . $option['fullname'] . ' </strong>' . $description . '</td>';
+
+            echo '</tr>';
+        }
+
+        echo '</table>';
+
+        echo '</div>';
+    }
+
+    public function generate_foodtype_shortcode()
+    {
+
+        $food_types = carbon_get_theme_option('wp_liefer_mildness_foodtype');
+
+        $this->generate_shortcode_table($food_types);
     }
 
     public function generate_additives_shortcode()
     {
         $additives = carbon_get_theme_option('wp_liefer_additives');
 
-        if (empty($additives)) {
-            return;
-        }
-
-        echo '<table class="additives">';
-
-        foreach ($additives as $key => $additive) {
-
-            $icon = wp_get_attachment_url($additive['icon']);
-
-            echo '<tr>';
-
-            echo '<td><img src="' . $icon . '" /></td>';
-
-            echo '<td> <strong> ' . $additive['fullname'] . ' </strong>' . $additive['description'] . '</td>';
-
-            echo '</tr>';
-        }
-
-        echo '</table>';
+        $this->generate_shortcode_table($additives);
     }
 
     public function generate_allergenes_shortcode()
     {
         $allergenes = carbon_get_theme_option('wp_liefer_alergenes');
 
-        if (empty($allergenes)) {
-            return;
-        }
-
-        echo '<table class="additives">';
-
-        foreach ($allergenes as $key => $allergene) {
-
-            $icon = wp_get_attachment_url($allergene['icon']);
-
-            echo '<tr>';
-
-            echo '<td><img src="' . $icon . '" /></td>';
-
-            echo '<td> <strong> ' . $allergene['fullname'] . ' </strong>' . $allergene['description'] . '</td>';
-
-            echo '</tr>';
-        }
-
-        echo '</table>';
+        $this->generate_shortcode_table($allergenes);
     }
 }
